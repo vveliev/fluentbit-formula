ccc30265ea16eb6c66d23ce69f650281badd946b
vveliev <velievv@gmail.com>
2023-03-03 10:59:09 -0500

    refactor: pre-commit style fixes

--- test/integration/default/controls/packages.rb
+++ test/integration/default/controls/packages.rb
@@ -1,20 +1,13 @@
 # frozen_string_literal: true
 
-# Prepare platform "finger"
-platform_finger = system.platform[:finger].split('.').first.to_s
+# # Prepare platform "finger"
+# platform_finger = system.platform[:finger].split('.').first.to_s
 
 control 'fluentbit.package.install' do
   title 'The required package should be installed'
 
   # Override by `platform_finger`
-  package_name =
-    case platform_finger
-    when 'centos-6', 'amazonlinux-1'
-      'cronie'
-    else
-      'bash'
-    end
-
+  package_name = 'fluendbit'
   describe package(package_name) do
     it { should be_installed }
   end
